534380c5d3b3d647c1a0c5d5dd27edaf08b01f63
homelesscat <gilbertperezdt@gmail.com>
2023-09-12 20:25:47 -0700

    functional application being pushed up for production

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@ const timerEl = document.querySelector("#timer");
 const quizEl = document.querySelector("#quiz");
 const startPageEl = document.querySelector("#start");
 const startBtnEl = document.querySelector("#start-button");
-const inputEl = document.querySelector("#user-input");
 const results = document.querySelector("#results");
-const submitBtn = document.querySelector("#submit");
-const endQuiz = false;
-const timeLeft = 60;
-const score = 0;
-const current = 0;
-const highScores = [];
-const shuffledQuestions = [];
-const shuffledAnswers = [];
+const scoreEl = document.querySelector("#your-score");
+const scoreDisplayEl = document.querySelector("#enter-score");
+let inputEl = document.querySelector("#user-input");
+let submitBtn = document.querySelector("#submit");
+let endQuiz = false;
+let timeLeft = 60;
+let score = 0;
+let current = 0;
+var highScores = [];
+var shuffledQuestions = [];
+var shuffledAnswers = [];
 
 const questions = [
   {
@@ -46,6 +48,7 @@ const questions = [
   },
 ];
 
+// Timer function ends quiz if time runs out
 const setTimer = () => {
   timerEl.textContent = "Time: " + timeLeft;
   var timerInterval = setInterval(function () {
@@ -62,3 +65,109 @@ const setTimer = () => {
     }
   }, 1000);
 };
+
+// Used to shuffle questions and answers
+function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var mix = Math.floor(Math.random() * (i + 1));
+    [array[i], array[mix]] = [array[mix], array[i]];
+  }
+  return array;
+}
+
+function runQuiz() {
+  // If there are no more questions, ends the quiz
+  if (current === questions.length || endQuiz) {
+    if (timeLeft > 0) {
+      score = timeLeft;
+    } else {
+      score = 0;
+    }
+    quizOver();
+    return;
+  }
+
+  // Decides which question is current and shuffles answer choices
+  var currentQuestion = shuffledQuestions[current];
+  shuffledAnswers = shuffle(currentQuestion.answers);
+
+  // Dynamically creates and appends question text and empty ul to page
+  var questionEl = document.createElement("h2");
+  var answersEl = document.createElement("ul");
+  questionEl.textContent = currentQuestion.text;
+  quizEl.appendChild(questionEl);
+  quizEl.appendChild(answersEl);
+
+  // For each answer choice, creates a new li and appends them to the page
+  for (var i = 0; i < shuffledAnswers.length; i++) {
+    var choice = document.createElement("li");
+    choice.textContent = shuffledAnswers[i];
+    answersEl.appendChild(choice);
+  }
+}
+
+// Decides if chosen answer is correct and adjusts time, then moves to next question
+function answerSelected(event) {
+  if (event.target.matches("li")) {
+    if (event.target.textContent === shuffledQuestions[current].correct) {
+      timeLeft += 10;
+      results.textContent = "Correct!";
+      setTimeout(function () {
+        results.textContent = "";
+      }, 1000);
+    } else {
+      timeLeft -= 15;
+      results.textContent = "Wrong!";
+      setTimeout(function () {
+        results.textContent = "";
+      }, 1000);
+    }
+    quizEl.innerHTML = "";
+    current++;
+    runQuiz();
+  }
+}
+
+// Changes display to Enter Score input
+function quizOver() {
+  timerEl.setAttribute("style", "display: none");
+  quizEl.setAttribute("style", "display: none");
+  scoreDisplayEl.setAttribute("style", "display: inline-block");
+  scoreEl.textContent = "Final Score: " + score;
+  endQuiz = false;
+}
+
+// Starts the quiz
+function startQuiz(event) {
+  event.preventDefault();
+  startPageEl.setAttribute("style", "display: none");
+  quizEl.setAttribute("style", "display: inline-block");
+  shuffledQuestions = shuffle(questions);
+  setTimer();
+  runQuiz();
+}
+
+startBtnEl.addEventListener("click", startQuiz);
+quizEl.addEventListener("click", answerSelected);
+
+// Saves score and input initials to local storage, alerts if no input
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (localStorage.getItem("scores")) {
+    highScores = JSON.parse(localStorage.getItem("scores"));
+  }
+
+  if (inputEl.value) {
+    var initials = inputEl.value;
+    var finalScore = {
+      initials: initials,
+      score: score,
+    };
+    highScores.push(finalScore);
+    localStorage.setItem("scores", JSON.stringify(highScores));
+    location.href = "highscores.html";
+  } else {
+    alert("You haven't entered your initials yet!");
+  }
+});
